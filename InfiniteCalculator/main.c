57cfdf6c59d470d93f97df67aa7efbb74a99212c
Sushant Patil <Sushant@sushants-macbook-pro.local>
2015-11-21 17:56:27 -0500

    printf file function added

--- InfiniteCalculator/main.c
+++ InfiniteCalculator/main.c
@@ -8,9 +8,31 @@
 
 #include <stdio.h>
 
+//Read a file from the end
+void printFile(FILE *fp)
+{
+    char c;
+    while((c = getc(fp)) != EOF)
+        printf("%c", c);
+}
+
+
+//Add nodes
+
+//print the list
+
 int main(int argc, const char * argv[]) {
+    
+    FILE *fp;
+    fp = fopen("number_1.txt","r");
+    printFile(fp);
+    printf("\n");
+    fp = fopen("number_2.txt","r");
+    printFile(fp);
+    
+    
     // insert code here...
-    printf("Hello, World!\n");
+    printf("\nHello, World!\n");
     printf("Awesome");
     return 0;
 }
